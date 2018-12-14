90d53579b574f38d8ecca258cf9435bb6bd23c46
Zynh0722 <Zynh0722@gmail.com>
2018-12-14 14:59:38 -0800

    Update sketch.js
    Added the ability to input: top input is for code, bottom input is for... Input

--- sketch.js
+++ sketch.js
@@ -15,20 +15,24 @@ function setup() {
   spinny = new wheel();
   main = new pent(1, 110, 175);
   acc = new accumulator(main, spinny, 10, 10);
-	interpreter = new interpreter(acc, main, spinny, ``, "Hello");
+	interpreter = new interpreter(acc, main, spinny, ``, "");
   
   input = createInput();
   input.size(150);
   input.position(565, 60 - 22.5);
   
+  otherInput = createInput();
+  otherInput.size(150);
+	otherInput.position(565, 60);
+  
   slider = createSlider(0.5, 100, 1, 0.5);
   slider.position(input.x, input.y - slider.height - 5);
   slider.style('width', '170px');
   
-  button = createButton('submit');
+  button = createButton('Submit');
   button.position(input.x + input.width, input.y);
-  button.mousePressed(setInput);
-
+  button.mousePressed(setSource);
+  
 }
 
 var count = 0;
@@ -40,8 +44,9 @@ function draw() {
   interpreter.step();
 }
 
-function setInput() {
+function setSource() {
   interpreter.reset();
+  interpreter.setIn(otherInput.value());
 	interpreter.inputSrc(input.value());
   interpreter.start();
 }
